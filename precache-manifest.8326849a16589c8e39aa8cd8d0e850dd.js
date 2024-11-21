ac06eb4219ec2368d5a5aebb4b30be5d389ce3b6
Bobkoskie, Robert (rb868x) <rb868x@att.com>
2024-11-21 11:30:56 -0500

    Updates

---
+++ precache-manifest.8326849a16589c8e39aa8cd8d0e850dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "24fb56b50710999ec423",
+    "url": "/react_flowers/static/css/main.c394f6fe.chunk.css"
+  },
+  {
+    "revision": "24fb56b50710999ec423",
+    "url": "/react_flowers/static/js/main.24fb56b5.chunk.js"
+  },
+  {
+    "revision": "2c80ac1fe2aa0d16567f",
+    "url": "/react_flowers/static/js/1.2c80ac1f.chunk.js"
+  },
+  {
+    "revision": "2b3bf5518c05b14b60a2",
+    "url": "/react_flowers/static/js/2.2b3bf551.chunk.js"
+  },
+  {
+    "revision": "dfa3d57745bcc2fc59d8",
+    "url": "/react_flowers/static/js/runtime~main.dfa3d577.js"
+  },
+  {
+    "revision": "31d8e79b96379832a534e9b305db0212",
+    "url": "/react_flowers/index.html"
+  }
+];
